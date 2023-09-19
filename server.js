5e269ddeefec1b44dce3158cf04187fd3c64d693
ArnoldPires <arnold.pires92@gmail.com>
2023-09-19 11:12:24 -0400

    Created a new page, to add new fruits

--- server.js
+++ server.js
@@ -2,15 +2,43 @@ const express = require("express");
 const app = express();
 const jsxEngine = require("jsx-view-engine");
 
+// Middleware
+app.use((req, res, next) => {
+  console.log('Run all the routes');
+  next();
+});
+
 const fruits = require("./models/fruits.js"); //NOTE: it must start with ./ if it's just a file, not an NPM package
 const vegetables = require("./models/vegetables.js"); // Import the vegetables array
 
 app.set("view engine", "jsx");
 app.engine("jsx", jsxEngine());
 
+// Tell express to use the middleware
+app.use(express.urlencoded({extended:false}));
+
+
+/*
+Fruits
+*/
 app.get("/fruits/", (req, res) => {
   // res.send(fruits);
   res.render("fruits/Index", { fruits: fruits });
+});
+
+app.get('/fruits/new', (req, res) => {
+  res.render('fruits/New');
+});
+
+app.post('/fruits', (req, res) => {
+  if (req.body.readyToEat === 'on') { // If checked, req.body.readyToEat is set to 'on'
+    req.body.readyToEat = true; //do some data correction
+  } else { //if not checked, req.body.readyToEat is undefined
+    req.body.readyToEat = false; //do some data correction
+  }
+  fruits.push(req.body);
+  console.log(req.body);
+  res.redirect('/fruits');
 });
 
 app.get("/fruits/:indexOfFruitsArray", (req, res) => {
@@ -21,14 +49,11 @@ app.get("/fruits/:indexOfFruitsArray", (req, res) => {
   }); // renders the info using the appropriate template
 });
 
+/*
+Vegetables
+*/
 app.get("/vegetables/", (req, res) => {
   res.render("vegetables/Index", { vegetables: vegetables });
-});
-
-app.get("/vegetables/:indexOfVegetablesArray", (req, res) => {
-  res.render("vegetables/Show", {
-    vegetable: vegetables[req.params.indexOfVegetablesArray],
-  });
 });
 
 app.get("/vegetables/new/", (req, res) => {
@@ -36,14 +61,20 @@ app.get("/vegetables/new/", (req, res) => {
 });
 
 app.post("/vegetables/new/", (req, res) => {
-    const newVegetable = {
-        name: req.body.name,
-        color: req.body.color,
-        readyToEat: req.body.readyToEat === "on",
-    }
-    vegetables.push(newVegetable);
-    res.redirect("/vegetables/");
+  const newVegetable = {
+    name: req.body.name,
+    color: req.body.color,
+    readyToEat: req.body.readyToEat === "on",
+  }
+  vegetables.push(newVegetable);
+  res.redirect("/vegetables/");
 })
+
+app.get("/vegetables/:indexOfVegetablesArray", (req, res) => {
+  res.render("vegetables/Show", {
+    vegetable: vegetables[req.params.indexOfVegetablesArray],
+  });
+});
 
 app.listen(3000, () => {
   console.log("listening to port 3000");
